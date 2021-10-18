f0b159162aa8b289f8f5e3d8c462ed6752781cc5
JohnnyWiseau141 <john.a.foley141@gmail.com>
2021-10-18 10:19:08 -0400

    add win message conditionals to render and begin handleClick

--- js/app.js
+++ js/app.js
@@ -17,9 +17,9 @@ const msgToPlayer = document.querySelector('.message')
 const resetGame = document.querySelector('#resetButton')
 
 //event listeners
-// cells.forEach((sec) => {
-//   sec.addEventListener('click' handleClick)
-// })
+cells.forEach((sec) => {
+  sec.addEventListener('click', handleClick)
+})
 
 resetGame.addEventListener('click', init)
 
@@ -31,12 +31,14 @@ resetGame.addEventListener('click', init)
 function init() {
 	resetGame.setAttribute('hidden', '')
   boardState = [
-    [null, null, null, null, null, null, null],
-    [1, null, null, null, null, null, 1],
-    [null, 1, null, null, null, null, null],
-    [null, null, 1, null, null, null, null],
-    [null, null, null, 1, null, null, null],
-    [null, null, 1, null, 1, null, 1]
+    [null, null, null, null, null, null, null], // [0]
+    [null, null, null, null, null, null, null], // [1]
+    [null, null, null, null, null, null, null], // [2]
+    [null, null, null, null, null, null, null], // [3]
+    [null, null, null, null, null, null, null], // [4]
+    [null, null, null, null, null, null, null]  // [5]
+
+//  [0],   [1],   [2],  [3],  [4],  [5],  [6]
   ]
 
   cells.innerHTML = ''
@@ -48,9 +50,17 @@ function init() {
   render()
 }
 
-function handleClick() {
+function handleClick(sqr) {
+	resetGame.removeAttribute('hidden')
+
   
-}
+  if (boardState[sqIdx] || isWinner) {
+
+  }
+
+  
+
+} 
 
 function render() {
   boardState.forEach(function (array, row) {
@@ -60,7 +70,22 @@ function render() {
       }
     })
   })
-  
+  if (!isWinner ) {
+    if (playerTurn === 1) {
+    msgToPlayer.innerText = 'Blue Player go!'
+    }else{
+    msgToPlayer.innerText = 'Red player go!'
+    }
+  } else if (isWinner === "T") {
+    msgToPlayer.innerText = 'Tie game!'
+  } else {
+    if (isWinner === 1){
+      msgToPlayer.innerText = 'Blue player wins!'
+    } else {
+      msgToPlayer.innerText = 'Red player wins!'
+    }
+		
+  }
 }
 
 function winnerIsYou() {
